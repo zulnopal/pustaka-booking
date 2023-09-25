b5ef66e967354b107919d20d797b3db67f340cb8
zulnopal6 <zulnopal6@gmail.com>
2023-09-25 16:36:54 +0700

    tambah data

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo"<h1>Perkenalkan</h1>";
+        echo"Nama saya Muhammad Naufal Zul Fauzi
+            Saya tinggal di Daerah Kulonprogo
+            saya suka bermain game mobile";
+    }
+}
