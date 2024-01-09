0a872dbf5dcd506df57b1f4fefcff281578031b8
zulnopal6 <zulnopal6@gmail.com>
2024-01-09 20:59:14 +0700

    menambahkan sisa tugas

---
+++ application/views/templates/aute_footer.php
@@ -0,0 +1,16 @@
+<!-- Bootstrap core JavaScript-->
+<script src="<?= base_url('assets/');?>vendor/jquery/jquery.min.js"></script>
+<script src="<?= base_url('assets/');?>vendor/bootstrap/js/bootstreap.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?= base_url('assets/'); ?> vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?= base_url('assets/'); ?>js/jb-admin-2.min.js"></script>
+<script>
+$('.alert-message').alert().delay(3000).slideUp('slow');
+</script>
+
+</body>
+
+</html>
